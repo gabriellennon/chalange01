9afee2c3347046919a735e4c1276b4c81ecfdc2b
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-05-24 15:45:04 -0300

    app final tasks list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
@@ -14,15 +15,40 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(newTaskTitle !== ''){
+      const dataTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+
+      setTasks(oldState => [...oldState, dataTask])
+
+    }else {
+      Alert.alert('Digite o nome da tarefa ✍🏽')
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+
+    //pegando meu estado anterior de tasks e setando o novo estado com filter
+    setTasks(oldState => oldState.filter(
+      task => {
+        if(task.id === id){
+          task.done = true;
+        }
+      }
+    ));
+    
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
